ea08e19e658a38d782153cbf8305e873e5502dab
Hafieddz <hasmy41@gmail.com>
2025-04-15 19:01:01 +0800

    feat : add connection to server

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const { json } = require("sequelize");
 
 const app = express();
+const PORT = process.env.PORT || 8001;
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -12,3 +13,7 @@ app.use(express.json());
 app.use(
   session({ secret: "kelompok3", saveUninitialized: true, resave: true })
 );
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT : ${PORT}`);
+});
