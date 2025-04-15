5e5152199888766beb3c48df1be1f47bf7f977a6
Hafieddz <hasmy41@gmail.com>
2025-04-15 19:06:49 +0800

    fix : add routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const { json } = require("sequelize");
+const router = require("./routes/index");
 
 const app = express();
 const PORT = process.env.PORT || 8001;
@@ -13,6 +14,7 @@ app.use(express.json());
 // app.use(
 //   session({ secret: "kelompok3", saveUninitialized: true, resave: true })
 // );
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT : ${PORT}`);
