5bdce4c721e2258ac13a5acf2a5ab283c1aa742c
Hafieddz <hasmy41@gmail.com>
2025-04-15 21:47:00 +0800

    fix : bind server to ipv4 and piv6

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () => {
+app.listen(PORT, "::", () => {
   console.log(`Server running on PORT : ${PORT}`);
 });
