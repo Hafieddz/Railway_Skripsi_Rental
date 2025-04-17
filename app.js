705aacb56c52a8105a65c10d7dce4271c46b8c41
Hafieddz <hasmy41@gmail.com>
2025-04-17 17:50:56 +0800

    feat : add handle error function to app files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const { json } = require("sequelize");
 const router = require("./routes/index");
+const handleError = require("./middlwares/handleError");
 
 const app = express();
 const PORT = process.env.PORT || 8001;
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(router);
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT : ${PORT}`);
