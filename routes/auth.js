0986dab335e6809c9d25652612786b797b1ed96a
Hafieddz <hasmy41@gmail.com>
2025-04-17 17:51:53 +0800

    feat : add login and verify otp routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,13 @@ const {
   checkEmail,
   sendOtp,
   verifyEmail,
+  login,
 } = require("../controllers/AuthController");
 
 const router = require("express").Router();
 
 router.post("/register", checkEmail, sendOtp, verifyEmail);
+router.post("/verify-otp", verifyEmail);
+router.post("/login", login);
 
 module.exports = router;
